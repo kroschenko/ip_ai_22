80a2a48b5c20fc45faa56f794b0ef9d16c515074
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2024-03-30 16:13:58 +0300

    build: change sc-machine branch

--- scripts/build_scp_machine.sh
+++ scripts/build_scp_machine.sh
@@ -3,7 +3,7 @@ set -fexeo pipefail
 
 if [ -z "${SCP_MACHINE_PATH}" ];
 then
-  source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"/../scp-machine/scripts/set_vars.sh
+  source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"/set_vars.sh
 fi
 
 cd "${SCP_MACHINE_PATH}" && ./scripts/build_scp_machine.sh "$@"
